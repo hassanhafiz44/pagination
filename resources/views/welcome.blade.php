fefb27083ee5126f5fdb024b93776cac343d0b61
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2023-02-08 13:59:07 +0500

    Displaying all users at once

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,26 @@
     @vite(['resources/js/app.js'])
 </head>
 
-<body>
+<body class="p-5">
+    <h1>Users</h1>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->created_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 
 </html>
